24c4813c73f4c3d701f6f4a396d528e9bdb47d42
madhav9757 <madhavsemwal9@gmail.com>
2025-04-20 21:00:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.get('/api/songs/:folder', (req, res) => {
     });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080; // Use Railway's dynamic port or default to 8080
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
